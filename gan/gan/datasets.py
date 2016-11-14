35cfd6e125417e14e25491ef0b008313d5c44abf
kzky <rkzfilter@gmail.com>
2016-11-14 22:43:40 +0900

    Add LFW data reader.

--- gan/gan/datasets.py
+++ gan/gan/datasets.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from chainer import cuda
+import glob
 
 class MNISTDataReader(object):
     """DataReader
@@ -23,21 +24,59 @@ class MNISTDataReader(object):
 
         print("Num. of samples {}".format(self._n_data))
         
-    def get_train_batch(self,):
-        """Return next batch data.
-
-        Return next batch data. Once all samples are read, permutate all samples.
-        
-        Returns
-        ------------
-        np.ndarray
-        """
+    def get_train_batch(self):
         # Read and reshape data
         beg = self._next_position
         end = self._next_position + self._batch_size
         batch_data = self.data[beg:end, :]
         bs = batch_data.shape[0]
         batch_data = (batch_data / 255.).astype(np.float32).reshape(bs, 28, 28)
+
+        # Reset pointer
+        self._next_position += self._batch_size
+        if self._next_position >= self._n_data:
+            self._next_position = 0
+
+            # Shuffle
+            idx = np.arange(self._n_data)
+            np.random.shuffle(idx)
+            self.data = self.data[idx]
+            
+        return batch_data
+
+class LFWDataReader(object):
+    """DataReader
+    """
+    def __init__(self,
+                     train_path=\
+                     "/home/kzk/datasets/lfw",
+                     batch_size=64):
+
+        # Read and concate data
+        data = []
+        fpaths = glob.glob(os.path.join(train_path, "*", "*"))
+        for fpath in fpaths:
+            img = cv2.imread(fpath)
+
+            # Center cropping
+            img = img[128-64:128+64, 128-64:128+64, :].transpose(2, 0, 1)
+            data.append(img)
+        data = np.array(data, dtype=np.uint8)
+
+        self._batch_size = batch_size
+        self._next_position = 0
+        self._n_data = len(self.data)
+
+        print("Num. of samples {}".format(self._n_data))
+        
+    def get_train_batch(self,):
+        # Read and reshape data
+        beg = self._next_position
+        end = self._next_position + self._batch_size
+        batch_data = self.data[beg:end, :]
+        bs = batch_data.shape[0]
+        batch_data = batch_data.astype(np.float32).reshape(bs, 128, 128)
+        batch_data = (batch_data - 128) / 255.
 
         # Reset pointer
         self._next_position += self._batch_size
