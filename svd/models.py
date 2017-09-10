e4e19b02ca18364c83b4599a5970fffea4e46f1a
kzky <rkzfilter@gmail.com>
2017-09-10 20:21:28 +0900

    Add log.

--- svd/models.py
+++ svd/models.py
@@ -57,6 +57,9 @@ def mnist_lenet_prediction_slim(image, scope="slim", rrate=0.75, test=False):
 
 def reduce_maps(inmaps, outmaps, rrate):
     maps = int(rrate * int(1. * inmaps*outmaps / (inmaps+outmaps)))
+    logger.info(
+        "Num.Parameters is reduced {}x{} -> {} by rrate".format(
+            inmaps, outmaps, maps, rrate))
     return maps
 
 def decompose_network_and_set_params(model_load_path, 
