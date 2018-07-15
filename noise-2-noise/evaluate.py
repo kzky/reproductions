cc5edc8793fd9b8086bb1e5986cdb54b1746496c
kzky <rkzfilter@gmail.com>
2018-07-16 01:47:58 +0900

    Add eval sample.

--- noise-2-noise/evaluate.py
+++ noise-2-noise/evaluate.py
@@ -18,12 +18,75 @@ from models import REDNetwork, Noise2NoiseNetwork, get_loss
 
 def evaluate(args):
     # Model
-    x = nn.Variable([args.batch_size, 3, args.ih, args.iw])
-    x_noise = nn.Variable([args.batch_size, 3, args.ih, args.iw])
-    
+    if args.net == "RED":
+        net = REDNetwork(layers=30, step_size=2)
+    elif args.net == "noise2noise":
+        net = Noise2NoiseNetwork()
+    x = nn.Variable([1, 3, args.ih, args.iw])
+    x.persistent = True
+    x_noise = nn.Variable([1, 3, args.ih, args.iw])
+    x_noise.persistent = True
+    x_recon = net(x_noise)
+    x_recon.persistent = True
 
     # Data iterator
-    di = data_iterator_kodak()
+    if args.val_dataset == "kodak":
+        ds = KodacDataSource()
+        di = data_iterator_kodak(ds)
+    elif args.val_dataset == "bsds300":
+        ds = BSDSDataSource("bsds300")
+        di = data_iterator(ds)
+    elif args.val_dataset == "bsds500":
+        ds = BSDSDataSource("bsds500")
+        di = data_iterator(ds)
+    elif args.val_dataset == "set5":
+        ds = BSDSDataSource("set5")
+        di = data_iterator(ds)
+    elif args.val_dataset == "set14":
+        ds = BSDSDataSource("set14")
+        di = data_iterator(ds)
+    else:
+        raise ValueError("{} is not supported.".format(args.val_dataset))
+
+    # Monitor
+    def normalize_method(x):
+        max_idx = np.where(x > 255)
+        min_idx = np.where(x < 0)
+        x[max_idx] = 255
+        x[min_idx] = 0
+        return x
+    monitor = Monitor(args.monitor_path)
+    monitor_loss = MonitorSeries("Reconstruction Loss", monitor, interval=10)
+    monitor_time = MonitorTimeElapsed("Training Time per Resolution", monitor, interval=10)
+    monitor_image_train_clean = MonitorImage("Image Train Clean", monitor,
+                                                 num_images=1, 
+                                                 normalize_method=normalize_method, 
+                                                 interval=1)
+    monitor_image_train_noisy = MonitorImage("Image Train Noisy", monitor,
+                                                 num_images=1, 
+                                                 normalize_method=normalize_method, 
+                                                 interval=1)
+    monitor_image_train_recon = MonitorImage("Image Train Recon", monitor,
+                                                 num_images=1, 
+                                                 normalize_method=normalize_method, 
+                                                 interval=1)
+
+    # Evaluate
+    for i in range(ds.size):
+        # Denoise
+        x_data, _ = di.next()
+        x.d = x_data
+        x_noise.d = apply_noise(x_data, args.noise_level, distribution=args.dist)
+        loss.forward(clear_buffer=True)
+
+        # Save
+        monitor_image_train_clean.add(i, x_data)
+        monitor_image_train_noisy.add(i, x_noise.d)
+        monitor_image_train_recon.add(i, x_recon.d)
+
+        # Clear memory
+        import nnabla_ext.cuda
+        nnabla_ext.cuda.clear_memory_cache()
 
 
 def main():
