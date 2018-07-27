ac477014e3bf4ab72f90679a3a87f4567c5dea60
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-07-28 07:12:53 +0900

    Add commands.

--- lapsrn/commands.sh
+++ lapsrn/commands.sh
@@ -17,4 +17,18 @@ screen -Sdm "adam-3" bash -c "python train.py -d 0 --solver Adam --lr 1e-3 --mon
 screen -Sdm "adam-4" bash -c "python train.py -d 2 --solver Adam --lr 1e-4 --monitor-path result/example_10"
 
 screen -Sdm "adam-3-bn" bash -c "python train.py -d 1 --solver Adam --lr 1e-3 --monitor-path result/example_11 --use-bn"
+
 screen -Sdm "adam-4-bn" bash -c "python train.py -d 1 --solver Adam --lr 1e-4 --monitor-path result/example_12 --use-bn"
+
+
+screen -Sdm "adam-4" bash -c "python train.py -d 1 --solver Adam --lr 1e-4 --monitor-path result/example_13 --use-shared"
+screen -Sdm "adam-4" bash -c "python train.py -d 2 --solver Adam --lr 1e-4 --monitor-path result/example_14"
+
+screen -Sdm "momentum" bash -c "python train.py -d 1 --solver Momentum --lr 1e-6 --monitor-path result/example_15 --use-shared  --use-bn"
+screen -Sdm "momentum" bash -c "python train.py -d 2 --solver Momentum --lr 1e-6 --monitor-path result/example_16  --use-bn"
+
+screen -Sdm "adam-4" bash -c "python train.py -d 1 -S 2 --solver Adam --lr 1e-4 --monitor-path result/example_17 --use-shared"
+screen -Sdm "adam-4" bash -c "python train.py -d 2 -S 2 --solver Adam --lr 1e-4 --monitor-path result/example_18"
+
+screen -Sdm "momentum" bash -c "python train.py -d 1 -S 2 --solver Momentum --lr 1e-6 --monitor-path result/example_19 --use-shared  --use-bn"
+screen -Sdm "momentum" bash -c "python train.py -d 2 -S 2 --solver Momentum --lr 1e-6 --monitor-path result/example_20  --use-bn"
