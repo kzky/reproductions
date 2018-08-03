4fde925cdce416438db11d16753b907d3ce2724a
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-03 22:09:07 +0900

    Add commands.

--- noise-2-noise/command.sh
+++ noise-2-noise/command.sh
@@ -2,7 +2,7 @@
 # UNet
 screen -Sdm "unet-gaussian" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_gaussian --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist gaussian --noise-level 50"
 
-screen -Sdm "unet-poisson" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_poisson --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist poisson --noise-level 50"
+screen -Sdm "unet-poisson" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_poisson --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist poisson --noise-level 50  --n-replica 8"
 
 screen -Sdm "unet-bernoulli" bash -c "python train.py -d 1 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_bernoulli --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist bernoulli --noise-level 0.95"
 
