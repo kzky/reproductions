564b9a4d19e4cdd89b8ba845ff7a09340b01127a
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-04 10:00:14 +0900

    Change commands.

--- noise-2-noise/command.sh
+++ noise-2-noise/command.sh
@@ -1,12 +1,12 @@
 
 # UNet
-screen -Sdm "unet-gaussian" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_gaussian --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist gaussian --noise-level 50"
+screen -Sdm "unet-gaussian" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_gaussian_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist gaussian --noise-level 50"
 
-screen -Sdm "unet-poisson" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_poisson --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist poisson --noise-level 50  --n-replica 8"
+screen -Sdm "unet-poisson" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_poisson_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist poisson --noise-level 50"
 
-screen -Sdm "unet-bernoulli" bash -c "python train.py -d 1 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_bernoulli --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist bernoulli --noise-level 0.95"
+screen -Sdm "unet-bernoulli" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_bernoulli_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist bernoulli --noise-level 0.95"
 
-screen -Sdm "unet-impulse" bash -c "python train.py -d 1 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_impulse --train-data-path /data/datasets/imagenet/val_data/tmpdir --net unet --noise-dist impulse --noise-level 0.95 --loss l0"
+screen -Sdm "unet-impulse" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_impulse_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir --net unet --noise-dist impulse --noise-level 0.95 --loss l0"
 
 
 # RED30
