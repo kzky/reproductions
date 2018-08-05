6c9f3cb8b9d115c71a4ba8e026e6b25c38106bc6
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-06 06:18:27 +0900

    Add todo.

--- lapsrn/models.py
+++ lapsrn/models.py
@@ -37,6 +37,7 @@ class BilinearUpsampleInitiazlier(I.BaseInitializer):
 BUI = BilinearUpsampleInitiazlier
 
 def convolution(x, maps, kernel=(3, 3), pad=(1, 1), stride=(1, 1), name=None):
+    #todo: he_backward is good?
     std = I.calc_normal_std_he_forward(x.shape[1], maps, kernel)
     initizlier = I.NormalInitializer(std)
     return PF.convolution(x, maps, kernel, pad, stride, name=name, with_bias=False)
