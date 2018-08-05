36671a4ce79a39f5f4c678f3be153909f87b6f3e
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-06 02:43:51 +0900

    Add commands.

--- lapsrn/commands.sh
+++ lapsrn/commands.sh
@@ -64,3 +64,6 @@ screen -Sdm "adam-35" bash -c "python train.py -d 1 -b 64 --solver Adam --lr 1e-
 
 screen -Sdm "adam-36" bash -c "python train.py -d 0 -b 64 --solver Adam --lr 1e-4 --monitor-path result/example_36 --decay-rate 0 --share-type across-pyramid --S 2"
 screen -Sdm "adam-37" bash -c "python train.py -d 1 -b 64 --solver Adam --lr 1e-4 --monitor-path result/example_37 --decay-rate 0 --share-type within-pyramid --S 2"
+
+
+screen -Sdm "adam-38" bash -c "python train.py -d 2 -b 64 --solver Adam --lr 1e-4 --monitor-path result/example_38 --decay-rate 0 --share-type within-pyramid --S 2 --R 1"
