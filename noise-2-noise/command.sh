54b68e96096c65ee7bd1746925ccf0fca281e728
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-06 04:25:01 +0900

    Fix.

--- noise-2-noise/command.sh
+++ noise-2-noise/command.sh
@@ -4,9 +4,9 @@ screen -Sdm "unet-gaussian" bash -c "python train.py -d 2 -c cudnn -b 1 --max-it
 
 screen -Sdm "unet-poisson" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_poisson_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist poisson --noise-level 50"
 
-screen -Sdm "unet-bernoulli" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_bernoulli_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist bernoulli --noise-level 0.95 --n-replica 16"
+screen -Sdm "unet-bernoulli" bash -c "python train.py -d 1 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_bernoulli_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist bernoulli --noise-level 0.95 --n-replica 16"
 
-screen -Sdm "unet-impulse" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_impulse_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir --net unet --noise-dist impulse --noise-level 0.95 --loss l0 --n-replica 16"
+screen -Sdm "unet-impulse" bash -c "python train.py -d 1 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_impulse_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir --net unet --noise-dist impulse --noise-level 0.95 --loss l0 --n-replica 16"
 
 
 # RED30
