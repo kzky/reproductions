9d77e98ec28c6808165db58127d31f8b728c1eb2
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-06 02:24:49 +0900

    Add eval commands.

--- noise-2-noise/command.sh
+++ noise-2-noise/command.sh
@@ -1,4 +1,4 @@
-
+# TRAIN
 # UNet
 screen -Sdm "unet-gaussian" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_gaussian_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist gaussian --noise-level 50"
 
@@ -11,4 +11,13 @@ screen -Sdm "unet-impulse" bash -c "python train.py -d 2 -c cudnn -b 1 --max-ite
 
 # RED30
 screen -Sdm "red30-gaussian" bash -c "python train.py -d 2 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/red30_gaussian_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net red30 --noise-dist gaussian --noise-level 50"
+
+
+
+# EVAL
+python evaluate.py -d 2 -c "cudnn" --noise-dist gaussian --noise-level 25 --monitor-path result/red30_gaussian_2 --net red30 --model-load-path result/red30_gaussian_2/param_140625.h5
+python evaluate.py -d 2 -c "cudnn" --noise-dist gaussian --noise-level 25 --monitor-path result/unet_gaussian_2 --net unet --model-load-path result/unet_gaussian_2/param_140625.h5
+python evaluate.py -d 2 -c "cudnn" --noise-dist poisson --noise-level 30 --monitor-path result/unet_poisson_2 --net unet --model-load-path result/unet_poisson_2/param_140625.h5
+python evaluate.py -d 2 -c "cudnn" --noise-dist bernoulli --noise-level 0.5 --monitor-path result/unet_bernoulli_2 --net unet --model-load-path result/unet_bernoulli_2/param_140625.h5
+python evaluate.py -d 2 -c "cudnn" --noise-dist impulse --noise-level 0.5 --monitor-path result/unet_impulse_2 --net unet --model-load-path result/unet_impulse_2/param_140625.h5
 
