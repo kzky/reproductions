e81269b11aaf4fd6a1f2606157b67a1b38c784f8
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-06 11:24:04 +0900

    Add commands.

--- noise-2-noise/command.sh
+++ noise-2-noise/command.sh
@@ -1,4 +1,4 @@
-# TRAIN
+# TRAIN (noisy)
 # UNet
 screen -Sdm "unet-gaussian" bash -c "python train.py -d 1 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_gaussian_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist gaussian --noise-level 50"
 
@@ -12,6 +12,20 @@ screen -Sdm "unet-impulse" bash -c "python train.py -d 1 -c cudnn -b 1 --max-ite
 # RED30
 screen -Sdm "red30-gaussian" bash -c "python train.py -d 1 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/red30_gaussian_2 --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net red30 --noise-dist gaussian --noise-level 50"
 
+
+# TRAIN (clean)
+# UNet
+screen -Sdm "unet-gaussian-clean" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_gaussian_clean --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist gaussian --noise-level 50 --n-replica 1"
+
+screen -Sdm "unet-poisson-clean" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_poisson_clean --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist poisson --noise-level 50 --n-replica 1"
+
+screen -Sdm "unet-bernoulli-clean" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_bernoulli_clean --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net unet --noise-dist bernoulli --noise-level 0.95 --n-replica 1"
+
+screen -Sdm "unet-impulse-clean" bash -c "python train.py -d 0 -c cudnn -b 4 --max-iter 156250 --save-interval 15625 --monitor-path ./result/unet_impulse_clean --train-data-path /data/datasets/imagenet/val_data/tmpdir --net unet --noise-dist impulse --noise-level 0.95 --loss l0 --n-replica 1"
+
+
+# RED30
+screen -Sdm "red30-gaussian" bash -c "python train.py -d 1 -c cudnn -b 1 --max-iter 156250 --save-interval 15625 --monitor-path ./result/red30_gaussian_clean --train-data-path /data/datasets/imagenet/val_data/tmpdir/ --net red30 --noise-dist gaussian --noise-level 50"
 
 
 # EVAL
