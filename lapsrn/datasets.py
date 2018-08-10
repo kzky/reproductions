cad72a63f7a9c765d7e0e7cdc9a9cad9346bf95e
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-11 08:19:18 +0900

    Use PIL for aligning the matlab bicubic.

--- lapsrn/datasets.py
+++ lapsrn/datasets.py
@@ -1,4 +1,4 @@
-import cv2
+from PIL import Image
 import numpy as np
 import io
 import os
@@ -28,30 +28,32 @@ def data_iterator_lapsrn(img_paths, batch_size=64, ih=128, iw=128,
 
 
     def load_func_train(i):
-        img = cv2.imread(imgs[i])
+        img = Image.open(imgs[i])
 
         # To YCbCr
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+        img = img.convert("YCbCr")
 
         # Resize in [0.5 1.0]
-        h, w, c = img.shape
+        h, w = img.size
         if h < ih or w < iw:
-            img = cv2.resize(img, (iw, ih), interpolation=cv2.INTER_CUBIC)
+            img = img.resize((ih, iw),  Image.BICUBIC)
         a = np.random.uniform(0.5, 1.0)
         h, w = int(h*a), int(w*a)
         if h > ih and w > iw:
-            img = cv2.resize(img, (w, h), interpolation=cv2.INTER_CUBIC)
+            img = img.resize((h, w),  Image.BICUBIC)
         
+        # Rotate in [0, 90, 180, 270]
+        k = np.random.choice([0, 90, 180, 270], size=1)
+        img = img.rotate(k) if k != 0 else img
+
         # Crop
-        h, w, c = img.shape
+        img = np.asarray(img)
+        h, w, _ = img.shape
         if h > ih and w > iw:
             rh = np.random.choice(np.arange(h - ih), size=1)[0]
             rw = np.random.choice(np.arange(w - iw), size=1)[0]
             img = img[rh:rh+ih, rw:rw+iw, :]
 
-        # Rotate in [0, 90, 180, 270]
-        k = np.random.choice([0, 90, 180, 270], size=1)
-        img = np.rot90(img, k=k, axes=(0, 1)) if k in [90, 180, 270] else img
 
         # Flip horizontally
         if np.random.randint(2):
