aef9c61475fc9b873f631942fb51a4e0a9b5f1a1
Kazuki.Yoshiyama <rkzfilter@gmail.com>
2018-08-14 09:03:12 +0900

    Fix semantic bug.

--- lapsrn/helpers.py
+++ lapsrn/helpers.py
@@ -116,9 +116,9 @@ def denormalize(x, de=255.0):
     return x * de
 
 
-def ycbcr_to_rgb(y, cr, cb):
+def ycbcr_to_rgb(y, cb, cr):
     imgs = []
-    imgs_ = np.concatenate([y, cr, cb], axis=3)
+    imgs_ = np.concatenate([y, cb, cr], axis=3)
     for img in imgs_.astype(np.uint8):
         img = Image.fromarray(img, "YCbCr").convert("RGB")
         img = np.asarray(img)
